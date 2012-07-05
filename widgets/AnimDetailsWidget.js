b2af38099dd7a48b8cbbeac627071c3665878c36
Christoph Kempfer <kempfch@gmx.de>
2012-07-05 14:01:37 +0200

    Thumbnail size fix

--- widgets/AnimDetailsWidget.js
+++ widgets/AnimDetailsWidget.js
@@ -56,7 +56,7 @@ require(["dojo/_base/declare", "dijit/_Widget", "dijit/_TemplatedMixin", "dijit/
 		    });
 		    // Add all Resources to the list
 		    for (var index in resourceList) {
-		        resStore.put({name: resourceList[index], label: "<img class='comboBoxImage' src='' style='background:url(" + ResourceManager.getResourceUrl(resourceList[index]) + ");' /><span class='comboBoxText'>" + resourceList[index] + "</span>"});
+		        resStore.put({name: resourceList[index], label: "<img class='comboBoxImage' src='' style='background:url(" + ResourceManager.getResourceUrl(resourceList[index]) + "); background-size: contain; background-repeat:no-repeat;' /><span class='comboBoxText'>" + resourceList[index] + "</span>"});
 		    }
 		    // Update the store of the combobox
  			this.comboBox.attr('store', resStore);
